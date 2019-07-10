dc1b6b086e00b137ff1c77bc72ea9130e901028a
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-10 14:52:08 +0800

    finish filter_2 choose_multiples_ofthree

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const condiction = (num)=>{
+  return num % 3 == 0;
+}
 function choose_multiples_of_three(collection) {
-
+  return collection.filter(condiction);
   //implement here
 }
 
